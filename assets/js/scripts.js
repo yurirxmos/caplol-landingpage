650cf88427fe447be88eca5439e76a010f77f06f
Yuri Ramos <yuriramos2406@gmail.com>
2023-05-25 13:58:50 -0300

    adição de dropdown na navbar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -134,3 +134,49 @@ function toggleFormInscricao() {
     formInscricao.style.display = "none";
   }
 }
+
+// Função para organizar a tabela
+function organizarTabela() {
+  // Seleciona a tabela pelo ID
+  var tabela = document.querySelector('.tabela-conteudo');
+
+  // Obtém todos os elementos <li> da tabela
+  var liItems = Array.from(tabela.getElementsByTagName('li'));
+
+  // Ordena os elementos com base no número de vitórias e tempo
+  liItems.sort(function(a, b) {
+    // Obtém os elementos de pontuação (vitórias) e tempo de cada <li>
+    var scoreA = parseInt(a.getElementsByClassName('score')[1].innerText);
+    var scoreB = parseInt(b.getElementsByClassName('score')[1].innerText);
+    var timeA = a.getElementsByClassName('placar')[0].innerText.split('|')[0].trim();
+    var timeB = b.getElementsByClassName('placar')[0].innerText.split('|')[0].trim();
+
+    // Compara o número de vitórias
+    if (scoreA > scoreB) {
+      return -1; // A vem antes de B
+    } else if (scoreA < scoreB) {
+      return 1; // B vem antes de A
+    } else {
+      // Compara o tempo se o número de vitórias for igual
+      if (timeA > timeB) {
+        return 1; // B vem antes de A
+      } else if (timeA < timeB) {
+        return -1; // A vem antes de B
+      } else {
+        return 0; // Nenhuma alteração na ordem
+      }
+    }
+  });
+
+  // Atualiza a tabela com os elementos reordenados
+  liItems.forEach(function(li, index) {
+    // Atualiza a posição no <li>
+    li.getElementsByTagName('b')[0].innerText = (index + 1) + '.';
+
+    // Move o <li> para a nova posição na tabela
+    tabela.appendChild(li);
+  });
+}
+
+// Chama a função para organizar a tabela
+organizarTabela();
