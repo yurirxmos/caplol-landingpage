4e24f2e6058af4d8635b0bf97c459b8ee907a69a
Yuri Ramos <yuriramos2406@gmail.com>
2023-07-20 22:37:00 -0300

    atualização na tabela

--- assets/js/script-mvp.js
+++ assets/js/script-mvp.js
@@ -19,7 +19,8 @@ document.addEventListener("DOMContentLoaded", function() {
     { nome: "HEZ magíc", pontos: 100, time: "HEZ", imagem: "./assets/img/ranked-positions/fill.png" },
     { nome: "Neninha", pontos: 100, time: "UNB", imagem: "./assets/img/ranked-positions/sup.png" },
     { nome: "INV João Justino", pontos: 100, time: "INV", imagem: "./assets/img/ranked-positions/top.png" },
-    { nome: "VDE Franzzera", pontos: 100, time: "VDE", imagem: "./assets/img/ranked-positions/mid.png" }
+    { nome: "VDE Franzzera", pontos: 100, time: "VDE", imagem: "./assets/img/ranked-positions/mid.png" },
+    { nome: "Zaraki Kenpachi", pontos: 100, time: "UNB", imagem: "./assets/img/ranked-positions/jungle.png" }
   ];
 
   // Ordenando os jogadores com base nos pontos
