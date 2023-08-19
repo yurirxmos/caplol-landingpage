04e28292a1d58ae1d16ac5848912775047f97692
Yuri Ramos <yuriramos2406@gmail.com>
2023-08-19 01:29:06 -0300

    atualização de dia de playoff

--- assets/js/script-mvp.js
+++ assets/js/script-mvp.js
@@ -17,19 +17,20 @@ document.addEventListener("DOMContentLoaded", function() {
     { nome: "VDE 369", pontos: 400, time: "VDE", imagem: "./assets/img/ranked-positions/top.png" },
     { nome: "OLY Hercules", pontos: 200, time: "OLY", imagem: "./assets/img/ranked-positions/jungle.png" },
     { nome: "HEZ magíc", pontos: 200, time: "HEZ", imagem: "./assets/img/ranked-positions/fill.png" },
-    { nome: "Neninha", pontos: 200, time: "UNB", imagem: "./assets/img/ranked-positions/sup.png" },
+    { nome: "Neninha", pontos: 300, time: "UNB", imagem: "./assets/img/ranked-positions/sup.png" },
     { nome: "INV João Justino", pontos: 100, time: "INV", imagem: "./assets/img/ranked-positions/top.png" },
     { nome: "VDE Franzzera", pontos: 100, time: "VDE", imagem: "./assets/img/ranked-positions/mid.png" },
     { nome: "Zaraki Kenpachi", pontos: 200, time: "UNB", imagem: "./assets/img/ranked-positions/jungle.png" },
     { nome: "HEZ Paçoquita xD", pontos: 200, time: "UNB", imagem: "./assets/img/ranked-positions/fill.png" },
     { nome: "Amante da loira", pontos: 100, time: "ZDC", imagem: "./assets/img/ranked-positions/sup.png" },
     { nome: "ZDC Taranis", pontos: 100, time: "ZDC", imagem: "./assets/img/ranked-positions/jungle.png" },
-    { nome: "Gstvh", pontos: 100, time: "UNB", imagem: "./assets/img/ranked-positions/adc.png" },
+    { nome: "Gstvh", pontos: 200, time: "UNB", imagem: "./assets/img/ranked-positions/adc.png" },
     { nome: "VDE Ori", pontos: 100, time: "VDE", imagem: "./assets/img/ranked-positions/adc.png" },
     { nome: "OnlinePlay", pontos: 100, time: "ZDC", imagem: "./assets/img/ranked-positions/top.png" },
     { nome: "INV Gonn", pontos: 100, time: "INV", imagem: "./assets/img/ranked-positions/jungle.png" },
     { nome: "CR7 VASCO", pontos: 100, time: "UMP", imagem: "./assets/img/ranked-positions/adc.png" },
-    { nome: "AKÏRAH", pontos: 100, time: "UMP", imagem: "./assets/img/ranked-positions/mid.png" }
+    { nome: "AKÏRAH", pontos: 100, time: "UMP", imagem: "./assets/img/ranked-positions/mid.png" },
+    { nome: "Ze Mangão", pontos: 100, time: "DEB", imagem: "./assets/img/ranked-positions/adc.png" }
   ];
 
   // Ordenando os jogadores com base nos pontos
